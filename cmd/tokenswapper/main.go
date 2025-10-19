d148d32d51a849f16b4f31fbdfa337116d77eaf3
Frédéric VIRICEL <99799464+xxxPOUPOUxxx@users.noreply.github.com>
2025-10-19 21:53:39 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/tokenswapper/main.go
@@ -0,0 +1,23 @@
+// cmd/tokenswapper/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "tokenswapper/internal/tokenswapper"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := tokenswapper.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
